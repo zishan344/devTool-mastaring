0b7ebacb6fc093b2dfad28b2b46a8f2858550d0e
zishan344 <zishanahmed344@gmail.com>
2022-03-05 11:14:39 +0600

    done the todays module practic 1

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const KEY = "15674931-a9d714b6e9d654524df198e00&q";
 
 // show images
 const showImages = (images) => {
+  console.log(images);
   imagesArea.style.display = "block";
   gallery.innerHTML = "";
   // show gallery title
